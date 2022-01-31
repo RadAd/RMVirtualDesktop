fd83ab45957636fc31f95c7d811d0caec1df2403
RadAd <adamgates84+github@gmail.com>
2022-01-31 22:39:28 +1100

    Use tag in package name

--- Utils/Package.bat
+++ Utils/Package.bat
@@ -1,9 +1,14 @@
 @setlocal
 echo off
 prompt $G$S
-set name=Virtual.Desktop.Plugin.Demo_1.1
+set _=%1
+if defined _ set _=_%_: =_%
+set name=Virtual.Desktop.Plugin.Demo%_%
 
 echo on
+msbuild VirtualDesktop.sln /p:Configuration=Release /p:Platformt=x86 /t:Rebuild || exit /b
+msbuild VirtualDesktop.sln /p:Configuration=Release /p:Platformt=x64 /t:Rebuild || exit /b
+
 @call :safemkdir Plugins
 @call :safemkdir Plugins\32Bit
 @call :safemkdir Plugins\64Bit
@@ -12,6 +17,7 @@ copy Bin\x64Release\VirtualDesktop.dll Plugins\64Bit || exit /b
 if exist %name%.zip del %name%.zip
 7z a %name%.zip Plugins Skins RMSKIN.ini
 del %name%.rmskin
+copy %name%.zip %name%.2.zip
 powershell -File %~dp0MakeRmsSkin.ps1 %name%.zip
 @goto :eof
 
